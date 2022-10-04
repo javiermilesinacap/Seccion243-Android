a1e4ed4479bf195946af4c9488232c8fd16accb2
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-04 17:05:29 -0300

    upgrade

--- app/src/main/java/com/example/seccion243/MainActivity.kt
+++ app/src/main/java/com/example/seccion243/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.seccion243
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
